a2334fa29e1785f660d5db73b10ad71a4ff79a4e
Stephen Brooks <stephen@persephone.localdomain>
2017-12-29 18:04:39 -0600

    big changes. it works local.

--- iort/hbs_api.py
+++ iort/hbs_api.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Author(s): Stephen Brooks
 
 import os
@@ -5,7 +6,7 @@ import sys
 import logging
 from flask import Flask
 
-
+COMMAND_FILE_PATH = r'/home/stephen/Projects/capstone-hhri/hbs/api/command'
 app = Flask(__name__)
 
 
@@ -16,39 +17,39 @@ def index():
 
 @app.route('/api/getcommand')
 def get_command():
-   if os.path.exists(r'/opt/hbs/api/command'):
-       with open(r'/opt/hbs/api/command', 'rb') as cmd_file:
-           return cmd_file.read(1)
+    if os.path.exists(COMMAND_FILE_PATH):
+       with open(COMMAND_FILE_PATH, 'rb') as cmd_file:
+            return cmd_file.read(1)
     else:
         return "Could not open command file"
 
 
 @app.route('/api/readsonar')
 def read_sonar():
-   if os.path.exists(r'/opt/hbs/api/command'):
-       with open(r'/opt/hbs/api/command', 'wb') as cmd_file:
-           cmd_file.write(b'\x01')
-        return 0
+    if os.path.exists(COMMAND_FILE_PATH):
+        with open(COMMAND_FILE_PATH, 'wb') as cmd_file:
+            cmd_file.write(b'\x01')
+        return 'OK'
     else:
         return "Could not write to command file"
 
 
 @app.route('/api/speak', methods=['POST'])
 def speak():
-   if os.path.exists(r'/opt/hbs/api/command'):
-       with open(r'/opt/hbs/api/command', 'wb') as cmd_file:
-           cmd_file.write(b'\x02')
-        return 0
+    if os.path.exists(COMMAND_FILE_PATH):
+        with open(COMMAND_FILE_PATH, 'wb') as cmd_file:
+            cmd_file.write(b'\x02')
+        return 'OK'
     else:
         return "Could not write to command file"
 
 
 @app.route('/api/shakehead')
 def shake_head():
-   if os.path.exists(r'/opt/hbs/api/command'):
-       with open(r'/opt/hbs/api/command', 'wb') as cmd_file:
-           cmd_file.write(b'\x03')
-        return 0
+    if os.path.exists(COMMAND_FILE_PATH):
+        with open(COMMAND_FILE_PATH, 'wb') as cmd_file:
+            cmd_file.write(b'\x03')
+        return 'OK'
     else:
         return "Could not write to command file"
 
---
+++ iort/hbs_remote.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# Author(s): Stephen Brooks
+
+import os
+import sys
+import requests
+import logging
+
+
+WELCOME = """
+Welcome to the Robo-Sense control terminal, sponsored by Dewritos:
+    "If you want to fuel your pro gaming workouts the right way, choose Dewritos!"
+
+Please enter a command:
+
+    1)  Detect distance of nearest object in front of the robot
+    2)  Speak
+    3)  Shake head
+    4)  Exit the program
+"""
+
+VALID_COMMANDS = {
+    1: 'readsonar',
+    2: 'speak',
+    3: 'shakehead'
+}
+
+
+def start_logging():
+    # log_format = logging.Formatter('%(asctime)s : %(levelname)s | %(name)s\t %(message)s')
+    logging.basicConfig(filename='robot_app.log',
+                        level=logging.DEBUG,
+                        format='%(asctime)s : %(levelname)s\t %(message)s')
+
+
+def send_command(user_input, tts=None):
+    logger = logging.getLogger(__name__)
+    
+    if tts is not None:
+        logger.info('Sending POST request with data %s ', tts)
+        req = requests.post('http://localhost:5000/api/speak', data={'speech':tts})
+    else:
+        logger.info('Sending GET request for \"%s\" command.', user_input)
+        req = requests.get('http://localhost:5000/api/{}'.format(VALID_COMMANDS.get(user_input)))
+    
+    return req.status_code
+
+
+def check_input(user_input):
+    logger = logging.getLogger(__name__)
+    
+    if user_input == 4:
+        logger.warning('User wishes to exit. Shutting down...')
+        print('Goodbye.')
+        sys.exit(0)
+    elif not user_input in VALID_COMMANDS:
+        logger.warning('User input %s is not a valid command.', user_input)
+        sys.stdout.write('Your input is not a valid command. Try again: ')
+    # elif not len(str(user_input)) == 1:
+    #     logger.warning('User input %s is not a valid command.', user_input)
+    #     print('Your input is not valid. Try again...')
+    else:
+        logger.info('User input %s was accepted.', user_input)
+        if user_input == 2:
+            sys.stdout.write('Please provide text for the robot to speak: ')
+            tts = input()
+            return send_command(user_input, tts)
+        else:
+            return send_command(user_input)
+    logger.error('Something went wrong...')
+    return None
+
+
+def main():
+    start_logging()
+    logger = logging.getLogger(__name__)
+    logger.info('Application has started.')
+    
+    print(WELCOME)
+    while True:
+        try:
+            sys.stdout.write('Command: ')
+            user_input = input()
+            resp = check_input(int(user_input))
+            if resp == None:
+                pass
+            elif resp != 200:
+                logger.error('The request was unsuccessful. Status code: %s ', resp)
+                print('The API call failed. Try again... ')
+        except ValueError:
+            logger.warning('User input %s is not a valid command.', user_input)
+            sys.stdout.write('Your input is not a valid command. Try again: ')
+            pass
+    return 0
+
+
+if __name__ == '__main__':
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        print('\nKeyboardInterrupt detected. Shutting down...')
+        sys.exit(0)
