193c24cae94312ff1fad136887510e7a3abce332
Stephen Brooks <sfb130030@utdallas.edu>
2018-01-16 18:34:06 -0600

    begin i2c_mgr.cpp

---
+++ nodes/service/i2c_mgr.cpp
@@ -0,0 +1,10 @@
+#include <sys/types.h>
+#include <queue>
+#include <stdint.h>
+
+std::queue<char>* R_QUEUE = new std::queue<char>;
+
+
+int main(uint8_t address, char req[25], char data[25], int datasize) {
+  return 1;
+}
