852fdc6525fe10fb3f4d657655f1f560c8d0d899
Stephen Brooks <sfb130030@utdallas.edu>
2018-01-23 11:52:51 -0600

    moved i2c_mgr.cpp

--- nodes/service/i2c_mgr.cpp
+++ nodes/service/i2c_mgr.cpp
@@ -1,10 +1,51 @@
 #include <sys/types.h>
-#include <queue>
-#include <stdint.h>
+#include <string>
+#include <ros/ros.h>
+#include "hbs2_1/i2cbus.h"
 
-std::queue<char>* R_QUEUE = new std::queue<char>;
+
+int fd;
+char* iic_dev = "/dev/i2c-1";
 
 
-int main(uint8_t address, char req[25], char data[25], int datasize) {
-  return 1;
+void write_req(uint8_t addr, char regs[25], char data[25], int datasize, char* res) {
+
+}
+
+void read_req(uint8_t addr, char regs[25], char data[25], int datasize, char* res) {
+    
+}
+
+void handle_req(hbs2_1::i2cbus::Request &req, hbs2_1::i2cbus::Response &res) {
+    if (argc != 4) { 
+        // add error message
+        return false;
+    }
+    
+    uint8_t addr = req.address;
+    char* req = req.request;
+    char* data = req.data;
+    int dsize = req.datasize;
+
+    if (req[0] == 'R') { 
+        if (read_req(addr, req, data, dsize, res.data)) { res.success = true; }
+        else { res.sucess = false; }
+    }
+    else if (req[0] == 'W') {
+        if (write_req(addr, req, data, dsize, res.data)) { res.success = true; } 
+        else { res.success = false; }
+    }
+    else { 
+        // add error message
+        res.success = false;
+    }
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "i2c_bus_mgr_srv")
+    ros::NodeHandle n;
+
+    ros::ServiceServer srv = n.advertiseService("i2c_mgr", handle_req)
+    ros:spin();        
+    return 0;
 }
