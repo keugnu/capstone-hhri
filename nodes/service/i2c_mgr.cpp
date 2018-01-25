4175d1ec28e3ada7fe23c405a9c87fc67c92f178
Stephen Brooks <sfb130030@utdallas.edu>
2018-01-25 17:50:04 -0600

    compiles in catkin. update 1

--- nodes/service/i2c_mgr.cpp
+++ nodes/service/i2c_mgr.cpp
@@ -1,51 +1,66 @@
+// System
 #include <sys/types.h>
 #include <string>
-#include <ros/ros.h>
-#include "hbs2_1/i2cbus.h"
+#include <vector>
+#include <queue>
 
+// ROS
+#include "ros/ros.h"
+#include "hbs2/i2c_bus.h"
 
 int fd;
-char* iic_dev = "/dev/i2c-1";
+std::string iic_dev = "/dev/i2c-1";
+std::queue work_queue;
 
 
-void write_req(uint8_t addr, char regs[25], char data[25], int datasize, char* res) {
+bool write_req(const std::vector<signed char>* req, const std::vector<signed char>* res) {
 
+    return true;
 }
 
-void read_req(uint8_t addr, char regs[25], char data[25], int datasize, char* res) {
-    
+bool read_req(const std::vector<signed char>* req, const std::vector<signed char>* res) {
+
+    return true;
 }
 
-void handle_req(hbs2_1::i2cbus::Request &req, hbs2_1::i2cbus::Response &res) {
-    if (argc != 4) { 
-        // add error message
-        return false;
+bool handle_req(hbs2::i2c_bus::Request &req, hbs2::i2c_bus::Response &res) {
+    if (req.request[0] == 0) { 
+        if (read_req(&req.request, &res.data)) {
+	    ROS_INFO("Read request to I2C bus (address: %d) succeeded.", req.request[1]);
+	    res.success = true;
+	}
+        else {
+	    ROS_INFO("Read request to I2C bus (address: %d) failed.", req.request[1]);
+	    res.success = false;
+	}
     }
-    
-    uint8_t addr = req.address;
-    char* req = req.request;
-    char* data = req.data;
-    int dsize = req.datasize;
 
-    if (req[0] == 'R') { 
-        if (read_req(addr, req, data, dsize, res.data)) { res.success = true; }
-        else { res.sucess = false; }
+    else if (req.request[0] == 1) {
+        if (write_req(&req.request, &res.data)) {
+	    ROS_INFO("Write request to I2C bus (address: %d) succeeded.", req.request[1]);
+	    res.success = true;
+	}
+        else { 
+	    ROS_INFO("Write request to I2C bus (address: %d) failed.", req.request[1]);
+	    res.success = false;
+	}
     }
-    else if (req[0] == 'W') {
-        if (write_req(addr, req, data, dsize, res.data)) { res.success = true; } 
-        else { res.success = false; }
-    }
+
     else { 
-        // add error message
-        res.success = false;
+        ROS_INFO("Request to I2C bus (type: %d) is not valid.", req.request[0]);
+	res.success = false;
     }
+
+    return true;
 }
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "i2c_bus_mgr_srv")
+    ros::init(argc, argv, "i2c_bus_mgr_srv");
     ros::NodeHandle n;
 
-    ros::ServiceServer srv = n.advertiseService("i2c_mgr", handle_req)
-    ros:spin();        
+    // TODO implement priority queue
+
+    ros::ServiceServer srv = n.advertiseService("i2c_srv", handle_req);
+    ros::spin();        
     return 0;
 }
