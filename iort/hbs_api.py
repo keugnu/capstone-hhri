7dd5a4fb5ab5ff72a6b906a0d063e77b6d74019b
Stephen Brooks <stephen@persephone.localdomain>
2018-01-29 21:08:17 -0600

    logger defined, introduce tts capability

--- iort/hbs_api.py
+++ iort/hbs_api.py
@@ -4,9 +4,10 @@
 import os
 import sys
 import logging
-from flask import Flask
+from flask import Flask, request
 
 COMMAND_FILE_PATH = r'/home/stephen/Projects/capstone-hhri/hbs/api/command'
+SPEECH_KW_PATH = r'/home/stephen/Projects/capstone-hhri/hbs/api/tts'
 app = Flask(__name__)
 
 
@@ -23,7 +24,18 @@ def get_command():
             return cmd_file.read(1)
     else:
         logger.error('Could not write to command file')
-        return "Could not open command file"
+        return b'\xFF'
+
+
+@app.route('/api/gettts')
+def get_tts():
+    if os.path.exists(SPEECH_KW_PATH):
+        with open(SPEECH_KW_PATH, 'r') as spch_file:
+            logger.info('Reading from tts file.')
+            return spch_file.readline()
+    else:
+        logger.error('Could not read from tts file.')
+        return b'\xFF'
 
 
 @app.route('/api/readsonar')
@@ -32,22 +44,24 @@ def read_sonar():
         with open(COMMAND_FILE_PATH, 'wb') as cmd_file:
             cmd_file.write(b'\x01')
             logger.info('Wrote x01 to command file.')
-        return 'OK'
+        return b'\x00'
     else:
         logger.error('Could not write to command file')
-        return "Could not write to command file"
+        return b'\xFF'
 
 
-@app.route('/api/speak', methods=['POST'])
+@app.route('/api/speak')
 def speak():
     if os.path.exists(COMMAND_FILE_PATH):
         with open(COMMAND_FILE_PATH, 'wb') as cmd_file:
             cmd_file.write(b'\x02')
             logger.info('Wrote x02 to command file.')
-        return 'OK'
+        with open(SPEECH_KW_PATH, 'w') as spch_file:
+            spch_file.write(request.args.get('tts'))
+        return b'\x00'
     else:
         logger.error('Could not write to command file')
-        return "Could not write to command file"
+        return b'\xFF'
 
 
 @app.route('/api/shakehead')
@@ -57,16 +71,16 @@ def shake_head():
         with open(COMMAND_FILE_PATH, 'wb') as cmd_file:
             cmd_file.write(b'\x03')
             logger.info('Wrote x03 to command file.')
-        return 'OK'
-    else:
+        return b'\x00'
+    else: 
         logger.error('Could not write to command file')
-        return "Could not write to command file"
+        return b'\xFF'
 
 
 if __name__ == '__main__':
     logging.basicConfig(filename='robot_api.log',
                         level=logging.DEBUG,
                         format='%(asctime)s : %(levelname)s\t %(message)s')
-    logging.getLogger(__name__)
+    logger = logging.getLogger(__name__)
     logger.info('API starting.')
     app.run(debug=True)
