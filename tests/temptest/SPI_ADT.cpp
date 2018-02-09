e09a0277618a58d42f34c2984dbee4e819858254
Stephen Brooks <sfb130030@utdallas.edu>
2018-02-08 22:11:33 -0600

    Trying to read device id

--- tests/temptest/SPI_ADT.cpp
+++ tests/temptest/SPI_ADT.cpp
@@ -18,62 +18,31 @@ spi_config_t spi_config;
 
 int main(int argc, char *argv[]) {
 
-    uint8_t lsb, msb;
-
     // Configure the SPI bus
     spi_config.mode = 0;
-    spi_config.speed = 1000000;
+    spi_config.speed = 10000;
     spi_config.delay = 0;
     spi_config.bits_per_word = 8;
 
     SPI *mySPI = new SPI("/dev/spidev3.0", &spi_config);
 
     if(mySPI->begin()) {
-	uint8_t config[3] = {0};
-	// Write to configuration Registers (0x18, 0x19)
-	config[0] = 0x90;
-	config[1] = 0x18;
-	config[2] = 0x01;
-	mySPI->write(config,3);
-
-	config[1] = 0x19;
-	config[2] = 0x11;
-	mySPI->write(config,3);
-
-	uint8_t data[1];
-	uint8_t reg[2];
-	reg[0] = 0x90;
-	reg[1] = 0x4D;
-	data[0] = 0x91;
+	uint8_t txBuffer[3] = {0x90, 0x4D, 0x00};
+	uint8_t rxBuffer[3] = {0x00, 0x00, 0x00};
 
 	// Device id register: 0x4D
 	// Reading  device id before attempting to read temperature
 	while(true) {
-	    mySPI->xfer(reg, 2, data, 1);
-      	    printf("Device ID: %u \n", data[0]);
-	    sleep(1);
+	    mySPI->xfer(txBuffer, 3, rxBuffer, 3);
+   	    printf("ID: %u - %u - %u \n", rxBuffer[2], rxBuffer[1], rxBuffer[0]);
+	    sleep(1);	
 	}
-
-/*	
-	mySPI->write(reg,1);
-	mySPI->read(data,1);
-	lsb = data[0]%4;
-
-	reg[0] = 0x07;
-	mySPI->write(reg,1);
-	mySPI->read(data,1);
-	msb = data[0];
-
-	// Print temperatures in C and F
-	float temp = ((msb << 2) + lsb) * 0.25;
-	float ftemp = temp * 1.8 + 32;
-	printf("ADT Temperature in Celsius: %.2f C \n", temp);
-	printf("ADT Temperature in Fahrenheit: %.2f F \n", ftemp);
-*/
 	delete mySPI;
     } else {
 	printf("Failed to access SPI bus. \n");
     }
 
     return 1;
+
 }
+
