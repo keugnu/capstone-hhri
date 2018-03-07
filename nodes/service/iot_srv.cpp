8ca1972915b45c7b0e63fb3d6ad7e3a0c1759f8a
Stephen Brooks <sfb130030@utdallas.edu>
2018-03-06 19:15:47 -0600

    syncing with workspace

--- nodes/service/iot_srv.cpp
+++ nodes/service/iot_srv.cpp
@@ -2,76 +2,79 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <iostream>
+#include <boost/bind.hpp>
 
 // ROS
 #include "ros/ros.h"
-#include "hbs2/iot_srv.h"
+#include "hbs2/iot.h"
+#include "hbs2/sonar.h"
+#include "hbs2/tts.h"
+#include "hbs2/servo.h"
 
 
+ros::NodeHandle* n = new ros::NodeHandle;
 
-void handle_request(hbs2::iot_srv::Request &req, hbs2::iot_srv::Response &res, ros::NodeHandle &n) {
+
+bool handle_req(hbs2::iot::Request &req, hbs2::iot::Response &res) {
     
-    ros::ServiceClient sonar_client = n.serviceClient<hbs2::sonar_hw>("sonar_srv");
-    ros::ServiceClient tts_client = n.serviceClient<hbs2::tts_hw>("tts_srv");
-    ros::ServiceClient servo_client = n.serviceClient<hbs2::servo_hw>("servo_srv");
+    ros::ServiceClient sonar_client = n->serviceClient<hbs2::sonar>("sonar_srv");
+    ros::ServiceClient tts_client = n->serviceClient<hbs2::tts>("tts_srv");
+    ros::ServiceClient servo_client = n->serviceClient<hbs2::servo>("servo_srv");
 
-    hbs2::sonar_hw srv_sonar;
-    hbs2::tts_hw srv_tts;
-    hbs2::servo_hw srv_servo;
+    hbs2::sonar srv_sonar;
+    hbs2::tts srv_tts;
+    hbs2::servo srv_servo;
 
     switch (req.command) {
         case 1: {
             ROS_INFO("Serving request to read sonar sensor.");
             sonar_client.call(srv_sonar);
             if (srv_sonar.response.success == true) {
-                res.response.success = true;
-                res.response.data = srv_sonar.response.data;
+                res.success = true;
+                res.data = srv_sonar.response.data;
             }
             else {
                 ROS_ERROR("Request to read sonar sensor failed in iot_srv.");
-                res.response.success = false;
+                res.success = false;
+                return false;
             }
             break;
         }
         case 2: {
             ROS_INFO("Serving request for TTS.");
-            srv_tts.request.text = req.request.text;
+            srv_tts.request.text = req.text;
             tts_client.call(srv_tts);
-            if (srv_tts.response.success == true) { res.response.success = true; }
+            if (srv_tts.response.success == true) { res.success = true; }
             else {
                 ROS_ERROR("TTS Request failed in iot_srv.");
-                res.response.success = false;
+                res.success = false;
+                return false;
             }
             break;
         }
         case 3: {
             ROS_INFO("Serving request to shake head.");
             servo_client.call(srv_servo);
-            if (srv_servo.response.success == true) { res.response.success = true; }
+            if (srv_servo.response.success == true) { res.success = true; }
             else {
                 ROS_ERROR("Request to shake head has failed in iot_srv.");
-                res.response.success = false;
+                res.success = false;
+                return false;
             }
             break;
         }
     }
+    return true;
 }
 
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "iot_srv");
-    ros::NodeHandle n;
+    ros::NodeHandle nh = *n;
 
-    ros::ServiceServer srv = n.advertiseService("iot_srv", handle_req);
-    ros::ServiceClient sonar_client = n.serviceClient<hbs2::sonar_hw>("sonar_srv");
-    ros::ServiceClient tts_client = n.serviceClient<hbs2::tts_hw>("tts_srv");
-    ros::ServiceClient servo_client = n.serviceClient<hbs2::servo_hw>("servo_srv");
+    ros::ServiceServer srv = n->advertiseService("iot_srv", handle_req);
 
-    hbs2::sonar_hw srv_sonar;
-    hbs2::tts_hw srv_tts;
-    hbs2::servo_hw srv_servo;
-
-    ROS_INFO("ROS IOT Service has started.");
+    ROS_INFO("ROS IoT Service has started.");
     ros::spin();
     return 0;
 }
