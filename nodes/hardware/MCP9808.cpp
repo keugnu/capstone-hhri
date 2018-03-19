203579b04bb4068303fb893a3a7db65725cdcde8
Stephen Brooks <sfb130030@utdallas.edu>
2018-03-18 19:36:07 -0500

    general updates

--- nodes/hardware/MCP9808.cpp
+++ nodes/hardware/MCP9808.cpp
@@ -1,4 +1,4 @@
-/* 	
+/*      
     MCP9808 client node that calls upon i2c_mgr.cpp service
     req format: [type, device addr, reg1, dat1, <dat1>,...,regN, datN, <datN>]
     if type = read req, dat1:N = 0
@@ -17,6 +17,18 @@
 #include "ros/ros.h"
 #include "hbs2/i2c_bus.h"
 
+
+bool status_req(ros::ServiceClient &client, hbs2::i2c_bus &srv) {
+    srv.request.request.resize(4);
+    srv.request.size = 4;
+    srv.request.request = {0x00, 0x18, 0x00, 0x00};
+    usleep(1000);
+    client.call(srv);
+    if (!srv.response.success) return false;
+    else return true;
+}
+
+
 // Configure sensor and set temperature resolution
 bool write_init(ros::ServiceClient &client, hbs2::i2c_bus &srv) {
     srv.request.request.resize(5);
@@ -24,15 +36,20 @@ bool write_init(ros::ServiceClient &client, hbs2::i2c_bus &srv) {
     srv.request.size = 5;
 
     if (client.call(srv)) {
+        /* wait for job to be served in the i2c manager. */
+        while(!status_req(client, srv));
     srv.request.request.resize(4);
     srv.request.request = {0x02, 0x18, 0x08, 0x03};
     srv.request.size = 4;
 
-    if (client.call(srv))
-        return true;    
+    if (client.call(srv)) {
+            /* wait for job to be served in the i2c manager. */
+            while(!status_req(client, srv));
+        return true;
+        }
     }
 
-    ROS_ERROR("Failed to call service i2c_srv");
+    ROS_ERROR("Failed to call service i2c_srv.");
     return false;
 }
 
@@ -43,14 +60,17 @@ float read_temp(ros::ServiceClient &client, hbs2::i2c_bus &srv) {
     srv.request.size = 4;
     
     if (client.call(srv)) {
-    std::vector<signed char> data(srv.response.data);
-    int temp = (srv.response.data.at(0) & 0x1F) * 256 + srv.response.data.at(1);
+        /* wait for job to be served in the i2c manager. */
+        while(!status_req(client, srv));
+
+    //std::vector<signed char> data(srv.response.data);
+    int temp = (((int)srv.response.data.at(0) & 0x1F) * 256 + (int)srv.response.data.at(1));
     if (temp > 4095) { temp -= 8192; }
 
-    float fTemp = (temp * 0.0625) * 1.8 + 32;
+    float fTemp = ((temp * 0.0625) * 1.8 + 32);
     return fTemp;
     }
-    ROS_ERROR("Failed to call service i2c_srv");
+    ROS_ERROR("Failed to call service i2c_srv.");
     return -1;
 }
 
@@ -62,9 +82,12 @@ int main(int argc, char **argv) {
     ros::NodeHandle n;
     ros::ServiceClient client = n.serviceClient<hbs2::i2c_bus>("i2c_srv");
     hbs2::i2c_bus srv;
+    ROS_INFO("ROS Node [mcp9808] has started.");
     if (write_init(client, srv)) {
-    float temperature = read_temp(client, srv);
-    ROS_INFO("Temperature in Fahrenheit: %.2f\n", temperature);
-}
-    return 0;	
+        while(1) {
+            usleep(1000000);
+            float temperature = read_temp(client, srv);
+        ROS_INFO("Temperature in Fahrenheit: %.2f\n", temperature);
+}       }
+    return 0;   
 }
